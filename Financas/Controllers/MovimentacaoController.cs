e6f044fdb710a3e40b4626f3ab44259e0d986b15
Alberto <albertomaronezio@hotmai.com>
2019-06-21 12:51:51 -0300

    Add Controller de Movimentação

---
+++ Financas/Controllers/MovimentacaoController.cs
@@ -0,0 +1,28 @@
+﻿using Financas.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    public class MovimentacaoController : Controller
+    {
+        private MovimentacaoDAO movimentacaoDAO;
+        private UsuarioDAO usuarioDAO;
+
+        public MovimentacaoController(MovimentacaoDAO movimentacaoDAO, UsuarioDAO usuarioDAO)
+        {
+            this.movimentacaoDAO = movimentacaoDAO;
+            this.usuarioDAO = usuarioDAO;
+        }
+
+        // GET: Movimentacao
+        public ActionResult Form()
+        {
+            ViewBag.Usuarios = usuarioDAO.Lista();
+            return View();
+        }
+    }
+}
---
+++ Financas/DAO/MovimentacaoDAO.cs
@@ -0,0 +1,29 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class MovimentacaoDAO
+    {
+        private FinancasContext context;
+
+        public MovimentacaoDAO(FinancasContext context)
+        {
+            this.context = context;
+        }
+
+        public void Adiciona(Movimentacao movimentacao)
+        {
+            context.Movimentacoes.Add(movimentacao);
+            context.SaveChanges();
+        }
+
+        public IList<Movimentacao> Lista()
+        {
+            return context.Movimentacoes.ToList();
+        }
+    }
+}
